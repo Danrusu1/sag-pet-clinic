ce1f0b4d7a42b4021aea069a14ad3a6bb8577342
rusud <d.rusu@house-of-communication.com>
2023-09-04 22:28:57 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/danrusu/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.danrusu.sfgpetclinic.model;
+
+/**
+ * Created by rusud on 04.09.2023
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/com/danrusu/sfgpetclinic/model/PetType.java
@@ -0,0 +1,16 @@
+package com.danrusu.sfgpetclinic.model;
+
+/**
+ * Created by rusud on 04.09.2023
+ */
+public class PetType {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
